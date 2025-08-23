4a7fd2b0c3de497462927162f31580855853e3e9
albertsh2000 <albertshahbazyan2000@gmail.com>
2025-08-23 15:47:52 +0400

    ...

--- vite.config.js
+++ vite.config.js
@@ -3,33 +3,6 @@ import react from "@vitejs/plugin-react";
 import { visualizer } from "rollup-plugin-visualizer";
 
 export default defineConfig({
+  base: "/",
   plugins: [react(), visualizer()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (!id) return;
-
-          if (id.includes("node_modules")) {
-            if (id.includes("react-dom") || id.includes("react")) {
-              return "react-vendor";
-            }
-            if (id.includes("react-router-dom")) {
-              return "react-router";
-            }
-            if (id.includes("firebase")) {
-              return "firebase";
-            }
-            if (id.includes("antd")) {
-              return "antd";
-            }
-            if (id.includes("zustand")) {
-              return "zustand";
-            }
-            return "vendor";
-          }
-        },
-      },
-    },
-  },
 });
